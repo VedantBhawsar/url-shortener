17681d629d8a8be57040541be1d000126a9bd630
Vedant Shankar Bhavsar <vedantbhavsar.a10@gmail.com>
2026-03-23 08:48:58 +0530

    convert react project into typescript and install tailwind css

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+
+function App () { 
+  return (
+    <div className='flex justify-center items-center w-full h-screen'> 
+      working on this project
+    </div>
+  )
+}
+
+export default App
